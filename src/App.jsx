d63899a4e178a27f7f2837e809189e00829986e3
BTC001B <btc001b.btcglobal@gmail.com>
2026-07-28 12:12:58 +0530

    username-fix-4

--- src/App.jsx
+++ src/App.jsx
@@ -1363,6 +1363,12 @@ function App() {
       }
       const digits = (username.match(/\d/g) || []).length;
       const letters = (username.match(/[a-z]/i) || []).length;
+      console.log({
+          username,
+          letters,
+          digits,
+          length: username.length
+      });
       if (digits < 3 || letters < 7) {
         setError('Email handle must be at least 7 letters and 3 numbers.');
         setView('signup-mail');
